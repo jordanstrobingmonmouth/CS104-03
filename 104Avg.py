aba6b75c6946eaf66907342527c2014244ac6613
jordanstrobingmonmouth <71049431+jordanstrobingmonmouth@users.noreply.github.com>
2020-09-24 17:53:17 -0400

    This is version 1 of my average program

---
+++ 104Avg.py
@@ -0,0 +1,13 @@
+numberOfScores = 0
+score1 = 0
+score2 = 0
+total = 0
+average = 0.0
+scoreCount = 0 
+
+
+#numberOfScores = input('Please enter the number of scores you want to average: ')
+score1 = int(input('Please enter a text score: '))
+score2 = int(input('Please enter a text score: '))
+average = (score1 + score2) / 2
+print(average)
