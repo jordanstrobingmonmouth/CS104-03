59e0a6058cec0ce447bd066c74d72a39b4f92213
jordanstrobingmonmouth <71049431+jordanstrobingmonmouth@users.noreply.github.com>
2020-10-01 13:27:07 -0400

    Average program with loop

--- 104Avg.py
+++ 104Avg.py
@@ -1,16 +1,15 @@
 numberOfScores = 0
-score1 = 0
-score2 = 0
+score = 0
 total = 0
 average = 0.0
 scoreCount = 0 
 
 numberOfScores = int(input('Please enter the number of scores you want to average: '))
 
-
-score = int(input('Please enter a score: '))
-total = total + score
-scoreCount = scoreCount + 1
+while numberOfScores != scoreCount:
+    score = int(input('Please enter a score: '))
+    total = total + score
+    scoreCount = scoreCount + 1
 
 
 average = total / numberOfScores
