f0698bc870bcf6543b8c6c63b19e1f2d068c79c6
jordanstrobingmonmouth <71049431+jordanstrobingmonmouth@users.noreply.github.com>
2020-11-24 13:20:52 -0500

    Add files via upload
    First commit

---
+++ PythonQueue.py
@@ -0,0 +1,16 @@
+names = []
+for x in range(1):
+   names.append("jordan")
+   names.append("michael")
+   names.append("brandon")
+   names.append("matthew")
+   names.append("nick")
+   names.append("neel")
+   names.append("nigel")
+   names.append("shaun")
+   names.append("johnathan")
+   names.append("trevor")
+   
+
+for x in range(len(names)):
+    print(names.pop(0))
